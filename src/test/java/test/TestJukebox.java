f5b4933a5576805a3e4e3db6253a7fb2d3ad9fc2
Lwaugh1 <Lwaugh1@github.com>
2021-09-24 13:27:28 +0100

    Change POM

--- src/test/java/test/TestJukebox.java
+++ src/test/java/test/TestJukebox.java
@@ -7,12 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestJukebox {
     Song Track1 = new Song("Bohemian Rhapsody", "Queen", "Rock", 355, 1975);
+    Song Track2 = new Song("Heroes", "David Bowie", "Rock", 212, 1977);
     Jukebox jukebox = new Jukebox();
 
     @Test
     public void testAddSong(){
         jukebox.addSong(Track1);
         assertEquals("Bohemian Rhapsody" ,jukebox.getSong(Track1), "The songs are not the same");
+    }
+    @Test
+    public void testDeleteSong(){
+        jukebox.deleteSong(Track1);
     }
 }
 
