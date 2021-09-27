67d6eda254f63a359ca56bd57a570372f0024832
Lwaugh1 <Lwaugh1@github.com>
2021-09-27 09:26:13 +0100

    Unit testing complete

--- src/test/java/test/TestJukebox.java
+++ src/test/java/test/TestJukebox.java
@@ -15,7 +15,19 @@ public class TestJukebox {
         jukebox.addSong(Track1);
         assertEquals("Bohemian Rhapsody" ,jukebox.getSong(Track1), "The songs are not the same");
     }
+    @Test
+    public void testDeleteSong(){
+        jukebox.addSong(Track1);
+        jukebox.addSong(Track2);
+        jukebox.deleteSong(Track1);
+        assertEquals(1, jukebox.quantity(), "too many items in jukebox");
+    }
+    @Test
+    public void testGetSong(){
+        jukebox.getSong(Track1);
+        assertEquals("Bohemian Rhapsody", jukebox.getSong(Track1), "The song titles were not the same");
 
+    }
     @Test
     public void testQuantity() {
         jukebox.addSong(Track1);
@@ -32,12 +44,6 @@ public class TestJukebox {
         jukebox.pauseSong(Track1);
         assertEquals("Paused " + Track1.getSongTitle(), jukebox.pauseSong(Track1), "the song did not pause");
     }
-    @Test
-    public void testDeleteSong(){
-        jukebox.addSong(Track1);
-        jukebox.addSong(Track2);
-        jukebox.deleteSong(Track1);
-        assertEquals(1, jukebox.quantity(), "too many items in jukebox");
-    }
+
 }
 
--- src/test/java/test/TestSong.java
+++ src/test/java/test/TestSong.java
@@ -5,5 +5,58 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestSong {
+    Song Track1 = new Song("Bohemian Rhapsody", "Queen", "Rock", 355, 1975);
+    Song Track2 = new Song("Cold Heart", "Elton John", "Dua Lipa","Pop", 202, 2021);
+    @Test
+    public void testGetSongTitle(){
+        Track1.getSongTitle();
+        assertEquals("Bohemian Rhapsody" ,Track1.getSongTitle(), "The songs are not the same");
+    }
 
+    @Test
+    public void testSetSongTitle(){
+        Track1.setSongTitle("Back in Black");
+        assertEquals("Back in Black", Track1.getSongTitle(), "The titles were not the same");
+    }
+
+    @Test
+    public void testGetArtist(){
+        Track1.getArtist();
+        assertEquals("Queen", Track1.getArtist(), "The artists were not the same");
+    }
+    @Test
+    public void testSetArtist(){
+        Track1.setArtist("ACDC");
+        assertEquals("ACDC", Track1.getArtist(), "The artists were not the same");
+    }
+    @Test
+    public void testGetFeat(){
+        Track2.getFeaturing();
+        assertEquals("Dua Lipa", Track2.getFeaturing(), "The feat artists were not the same");
+    }
+    @Test
+    public void testSetFeat(){
+        Track2.setFeaturing("PNAU");
+        assertEquals("PNAU", Track2.getFeaturing(), "The feat artists were not the same");
+    }
+    @Test
+    public void testGetGenre(){
+        Track1.getGenre();
+        assertEquals("Rock", Track1.getGenre(), "The genres were not the same");
+    }
+    @Test
+    public void testSetGenre(){
+        Track2.setGenre("Dance");
+        assertEquals("Dance", Track2.getGenre(), "The genres were not the same");
+    }
+    @Test
+    public void testGetDuration(){
+        Track1.getDuration();
+        assertEquals(355, Track1.getDuration(), "The durations were not the same");
+    }
+    @Test
+    public void testSetDuration(){
+        Track2.setDuration(180);
+        assertEquals(180, Track2.getDuration(), "The durations were not the same");
+    }
 }
